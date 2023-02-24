7e147d38bbc35762012c952a0ea2b8e43f967074
Danila <levafork@gmail.com>
2023-02-24 18:22:12 +0300

    Реализовал обработку номера телефона с помощью регулярного выражения

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pprint import pprint
 import csv
+import re
 with open("phonebook_raw.csv", encoding="utf-8") as f:
     rows = csv.reader(f, delimiter=",")
     contacts_list = list(rows)
@@ -15,4 +16,17 @@ for string in contacts_list:
     for i in range(0, 3):
         string[i] = initials[i]
 
-pprint(contacts_list)
+    # Шаблоны для номера телефона и добавочного номера
+    phone_pattern = r'((8|\+7)[\- ]?)?(\(?(\d{3})\)?[\- ]?)?([\d]{3})[\- ]*([\d]{2})[\- ]*([\d]{2})' \
+                    r'([\s(]*доб.\s*(\d+)\)?)?'
+    ext_phone_pattern = r'([\s(]?доб.\s*(\d+)\)?)'
+    ext_phone = ''
+
+    ext_find_res = re.findall(ext_phone_pattern, string[5])
+    if ext_find_res:
+        ext_phone = f' доб.{ext_find_res[0][1]}'
+
+    substitution = r'+7(\4)\5-\6-\7'
+    string[5] = re.sub(phone_pattern, substitution, string[5]) + ext_phone
+
+#pprint(contacts_list)
